817005a58d5695bca4b1c59363559afa1a3e3210
Peter <petershim61@gmail.com>
2015-09-09 21:02:41 -0700

    worked on google markers

--- www/js/app.js
+++ www/js/app.js
@@ -3,27 +3,74 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngMockE2E'])
 
-  .run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if(window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      }
-      if(window.StatusBar) {
-        StatusBar.styleDefault();
-      }
-    });
-  })
-  .config(function($stateProvider, $urlRouterProvider) {
+  // .run(function($ionicPlatform) {
+  //   $ionicPlatform.ready(function() {
+  //     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+  //     // for form inputs)
+  //     if(window.cordova && window.cordova.plugins.Keyboard) {
+  //       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  //     }
+  //     if(window.StatusBar) {
+  //       StatusBar.styleDefault();
+  //     }
+  //   });
+  // })
+  .run(function($httpBackend){
+    $httpBackend.whenGET('http://localhost:8100/valid')
+          .respond({message: 'This is my valid response!'});
+    $httpBackend.whenGET('http://localhost:8100/notauthenticated')
+          .respond(401, {message: "Not Authenticated"});
+    $httpBackend.whenGET('http://localhost:8100/notauthorized')
+          .respond(403, {message: "Not Authorized"});
+
+    $httpBackend.whenGET(/templates\/\w+.*/).passThrough();
+   })
+  .config(function($stateProvider, $urlRouterProvider, USER_ROLES) {
       $stateProvider
-          .state('map', {
-              url: '/map',
-              templateUrl: 'templates/map.html',
-              controller: 'MapCtrl'
+          .state('login', {
+              url: '/login',
+              templateUrl: 'templates/login.html',
+              controller: 'LoginCtrl'
+          })
+          .state('main', {
+              url: '/',
+              abstract: true,
+              templateUrl: 'templates/main.html'
+          })
+          .state('main.login', {
+              url: 'main/login',
+              views: {
+                'login-tab':{
+                  templateUrl: 'templates/login.html',
+                  controller: 'LoginCtrl'
+                }
+              }
+          })
+          .state('main.subscribed', {
+              url: 'main/subscribed',
+              views: {
+                'admin-tab':{
+                  templateUrl: 'templates/subscribed.html'
+                }
+              },
+            data: {
+              authorizedRoles: [USER_ROLES.subscribed]
+            }
+          })
+          .state('main.map', {
+              url: 'main/map',
+              views: {
+                'public-tab':{
+                  templateUrl: 'templates/map.html',
+                  controller: 'MapCtrl'
+                }
+              }
           });
+      $urlRouterProvider.otherwise(function ($injector, $location) {
+        var $state = $injector.get("$state");
+        $state.go("main.login");
+      });
 
-      $urlRouterProvider.otherwise('/map');
   });
--- www/js/controller.js
+++ www/js/controller.js
@@ -1,4 +1,81 @@
+var posArray = [];
+var locations = [
+      ['Bondi Beach', -33.890542, 151.274856, 4],
+      ['Coogee Beach', -33.923036, 151.259052, 5],
+      ['Cronulla Beach', -34.028249, 151.157507, 3],
+      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+      ['Maroubra Beach', -33.950198, 151.259302, 1]
+    ];
 angular.module('starter.controllers', ['ionic'])
+  .controller('AppCtrl', function($scope, $state, $ionicPopup, AuthService, AUTH_EVENTS){
+    $scope.username = AuthService.username();
+
+    $scope.$on(AUTH_EVENTS.notAuthorized, function(event) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Unauthorized!',
+        template: 'You are not allowed to access this resource.'
+      });
+    });
+
+    $scope.$on(AUTH_EVENTS.notAuthenticated, function(event) {
+      AuthService.logout();
+      $state.go('login');
+      var alertPopup = $ionicPopup.alert({
+        title: 'Session Lost!',
+        template: 'Sorry, You have to login again.'
+      });
+    });
+
+    $scope.setCurrentUsername = function(name) {
+      $scope.username = name;
+    };
+  })
+  .controller('LoginCtrl', function($scope, $state, $ionicPopup, AuthService){
+    $scope.data = {};
+
+    $scope.login = function(data) {
+      AuthService.login(data.username, data.password).then(function(authenticated) {
+        $state.go('main.map', {}, {reload: true});
+        $scope.setCurrentUsername(data.username);
+      }, function(err) {
+        var alertPopup = $ionicPopup.alert({
+          title: 'Login failed!',
+          template: 'Please check your credentials!'
+        });
+      });
+    };
+  })
+  .controller('MainCtrl', function($scope, $state, $http, $ionicPopup, AuthService){
+    $scope.logout = function() {
+      AuthService.logout();
+      $state.go('login');
+    };
+
+    $scope.performValidRequest = function() {
+    $http.get('http://localhost:8100/valid').then(
+      function(result) {
+        $scope.response = result;
+      });
+    };
+
+    $scope.performUnauthorizedRequest = function() {
+      $http.get('http://localhost:8100/notauthorized').then(
+        function(result) {
+          // No result here..
+        }, function(err) {
+          $scope.response = err;
+        });
+    };
+
+    $scope.performInvalidRequest = function() {
+      $http.get('http://localhost:8100/notauthenticated').then(
+        function(result) {
+          // No result here..
+        }, function(err) {
+          $scope.response = err;
+        });
+    };
+  })
   .controller('MapCtrl', ['$scope', function($scope, $ionicPopup) {
   // Code will be here
   $scope.user = {};
@@ -15,7 +92,10 @@ angular.module('starter.controllers', ['ionic'])
     var lgt = $scope.user.longitude;
     var des = $scope.user.desc;
 
-    console.log('click', lat, lgt);
+    console.log('current click', lat, lgt);
+    posArray.push({lat, lgt});
+
+
 
 
     // Code to write to Firebase will be here
@@ -31,22 +111,48 @@ angular.module('starter.controllers', ['ionic'])
             var lat = scope.$eval(attrs.lat);
             var lng = scope.$eval(attrs.lng);
 
+            // var data = [];
+
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                  var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                  };
+                  console.log(pos);
+
+                  map.setCenter(pos);
+                }, function() {
+                  handleLocationError(true, map.getCenter());
+                });
+              } else {
+                // Browser doesn't support Geolocation
+                handleLocationError(false, map.getCenter());
+              }
+
+
+            var marker;
+
             var myLatlng = new google.maps.LatLng(lat,lng),
             mapOptions = {
                   zoom: zValue,
                   center: myLatlng
               },
-            map = new google.maps.Map(element[0],mapOptions),
-            marker = new google.maps.Marker({
-                position: myLatlng,
-                map: map,
-                draggable:true
-            });
+            map = new google.maps.Map(element[0],mapOptions);
+            for(var i = 0; i < locations.length; i++){
+              marker = new google.maps.Marker({
+                  position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                  map: map,
+                  draggable:true
+              });
+            }
+
             google.maps.event.addListener(marker, 'dragend', function(evt){
               scope.$parent.user.latitude = evt.latLng.lat();
       		    scope.$parent.user.longitude = evt.latLng.lng();
       		    scope.$apply();
               console.log('Current Latitude:',evt.latLng.lat(),'Current Longitude:',evt.latLng.lng());
+
             });
         }
     };
